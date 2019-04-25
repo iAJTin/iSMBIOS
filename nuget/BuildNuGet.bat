5ab340002e00d33628bb4c9d9b775792ae965377
Fernando Garcia <fdo.garcia.vega@gmail.com>
2019-04-25 12:14:49 +0200

    Update nuget package to v1.0.5, equals to v.1.0.4 but without errors

--- nuget/BuildNuGet.bat
+++ nuget/BuildNuGet.bat
@@ -1,7 +1,5 @@
 @ECHO OFF
 CLS
 
-..\src\.nuget\nuget Pack iSMBIOS.1.0.4.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
-
-
+..\src\.nuget\nuget Pack iSMBIOS.1.0.5.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
 

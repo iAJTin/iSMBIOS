d1e1d20ccab8dcf8dd50c021ebf78cdbabe3fbbc
Fernando Garcia <fdo.garcia.vega@gmail.com>
2019-05-30 00:05:54 +0200

    Create v1.0.6

--- nuget/BuildNuGet.bat
+++ nuget/BuildNuGet.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 CLS
 
-..\src\.nuget\nuget Pack iSMBIOS.1.0.5.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
+..\src\.nuget\nuget Pack iSMBIOS.1.0.6.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
 

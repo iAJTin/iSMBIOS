845a4c09e3abf5d07b11e537882ff04bc13cc3b1
Fernando Garcia <fdo.garcia.vega@gmail.com>
2019-08-13 02:35:54 +0200

    update nuget package

--- nuget/BuildNuGet.bat
+++ nuget/BuildNuGet.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 CLS
 
-..\src\.nuget\nuget Pack iSMBIOS.1.0.6.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
+..\src\.nuget\nuget Pack iSMBIOS.1.0.7.nuspec -NoDefaultExcludes -NoPackageAnalysis -OutputDirectory ..\deployment\nuget
+
+pause
 

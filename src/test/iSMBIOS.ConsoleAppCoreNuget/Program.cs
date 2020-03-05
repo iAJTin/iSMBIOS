e578a3b039903d6e6bd19e917f5218292277a423
Fernando Garcia <fdo.garcia.vega@gmail.com>
2020-03-05 21:42:35 +0100

    ConsoleAppCoreNuget

---
+++ src/test/iSMBIOS.ConsoleAppCoreNuget/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace iSMBIOS.ConsoleAppCoreNuget
+{
+    using iTin.Core.Hardware;
+    using iTin.Core.Hardware.Specification;
+    using iTin.Core.Hardware.Specification.Dmi;
+    using iTin.Core.Hardware.Specification.Dmi.Property;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DmiStructureCollection structures = DMI.Instance.Structures;
+
+            // type 0
+            string biosVersion = GetSmbiosProperty(structures, DmiProperty.Bios.BiosVersion);
+            string biosVersionTyped = structures.GetProperty<string>(DmiProperty.Bios.BiosVersion);
+        }
+
+        private static string GetSmbiosProperty(DmiStructureCollection structure, IPropertyKey key)
+        {
+            string result = null;
+
+            result = structure.GetProperty(key).ToString();
+
+            return result;
+        }
+    }
+}
